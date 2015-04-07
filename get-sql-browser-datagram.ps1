ccf31b7184d4661ae26f2e411e5d155d72dc5b87
Thomas Stringer <sqlsalt@outlook.com>
2015-04-07 09:40:26 -0400

    Initial commit

---
+++ get-sql-browser-datagram.ps1
@@ -0,0 +1,32 @@
+function Get-SqlBrowserDatagram {
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]$MachineName = $env:COMPUTERNAME
+    )
+
+    $UdpClient = New-Object System.Net.Sockets.UdpClient
+    $UdpClient.Client.ReceiveTimeout = 30000
+
+    $UdpClient.Connect($MachineName, 1434)
+
+    $Encoding = New-Object System.Text.ASCIIEncoding
+    $ByteArray = 0x02
+
+    $UdpClient.Send($ByteArray, $ByteArray.Length) |
+        Out-Null
+
+    $RemoteEndpoint = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Any, 0)
+
+    try {
+        $BytesReceived = $UdpClient.Receive([ref] $RemoteEndpoint)
+        $StringReceived = $Encoding.GetString($BytesReceived)
+
+        $StringReceived.Replace(";", "`r`n")
+    }
+    catch {
+        Write-Error $_.Exception
+    }
+    finally {
+        $UdpClient.Dispose()
+    }
+}
